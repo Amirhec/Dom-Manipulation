41fe15da7da8741818da23c5b7021cb471cb47f7
Amir Lamrabat <secretsofthecode9@gmail.com>
2023-11-12 16:44:33 +0100

    styling

--- dom.js
+++ dom.js
@@ -1,7 +1,24 @@
-const container = document.querySelector('#container');
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
+const container = document.querySelector("#container")
+const paragraph = document.createElement("p")
+paragraph.textContent = "Hey im red"
+paragraph.style.cssText = "color: red;"
+container.appendChild(paragraph)
+const header = document.createElement('h3')
+header.textContent = 'Im a blue h3!'
+header.style.cssText = 'color: blue;'
+container.appendChild(header)
 
-container.appendChild(content);
+
+
+const content = document.createElement('div')
+content.classList.add("content")
+content.style.cssText = "border: 10px solid black; background-color: pink"
+const secondHeader = document.createElement("h1")
+secondHeader.textContent = "Im in a div"
+const secondParagraph = document.createElement("p")
+secondParagraph.textContent = "ME TOO!"
+content.appendChild(secondHeader)
+content.appendChild(secondParagraph)
+
+container.appendChild(content)
