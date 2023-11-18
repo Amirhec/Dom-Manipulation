9d68781905608c90d4b936515ddb6f45d386c083
Amir Lamrabat <secretsofthecode9@gmail.com>
2023-11-18 22:18:36 +0100

    object

--- object.js
+++ object.js
@@ -1,14 +1,11 @@
-function isEmpty(obj) {
-    if (Object.values(obj).length ===0){
-        return true 
-    }
-
-    return false 
-  }
-  
-  let schedule = {
-    name: 
+const person = {
+    name: ["Bob", "Smith"],
+    age: 32,
+    bio: function () {
+      console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+    },
+    introduceSelf: function () {
+      console.log(`Hi! I'm ${this.name[0]}.`);
+    },
   };
-  
-  console.log(isEmpty(schedule));
   
